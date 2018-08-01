f163a798983ad8a91cd82db9e86715c3440fb216
antoinelt <antoine.latorre.61@gmail.com>
2018-08-01 16:49:58 +0200

    New view added to the main window: - Creation of index.html - Add listener on button to trigger Electron method

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ const {
   BrowserWindow,
   Tray,
   Menu,
-  MenuItem
+  MenuItem,
+  ipcMain,
+  crashReporter
 } = electron;
 
 i18n.configure({
@@ -17,9 +19,14 @@ i18n.configure({
   directory: __dirname + '/locales'
 });
 
-// How to set locale of the app context and use JSON dictionnary
-// i18n.setLocale('fr');
-// console.log(i18n.__("tray.contextMenu.restart"));
+// Crash report in case of bad behaviour
+app.setPath('temp', __dirname + '/temp');
+crashReporter.start({
+  productName: 'TaskApp',
+  companyName: 'xPlatform.rocks',
+  submitURL: 'http://localhost:3000/api/app-crashes',
+  uploadToServer: false
+});
 
 app.on('ready', () => {
   const {
@@ -30,13 +37,23 @@ app.on('ready', () => {
   const win = new BrowserWindow({
     icon: 'src/img/tray.png',
     width: 300,
-    height: 400,
+    height: 500,
     resizable: false,
+    minimizable: false,
     show: false,
     alwaysOnTop: true,
     skipTaskbar: true,
     frame: false
   });
+  win.setMenu(null);
+  win.loadURL(`file://${__dirname}/views/index.html`);
+
+
+  // Check if there is a hyphen inside app.getLocale() result
+  // Delete everything after this hyphen if it exists
+  // Then set the i18n locale value with the one without hyphen
+  var idx = app.getLocale().indexOf("-");
+  (idx !== -1) ? i18n.setLocale(app.getLocale().slice(0, idx)): i18n.setLocale(app.getLocale());
 
   let contextMenu = new Menu();
   contextMenu.append(new MenuItem({
@@ -57,8 +74,11 @@ app.on('ready', () => {
 
   trayIcon.on('click', () => {
     win.setPosition(width - win.getSize()[0] - 1, height - win.getSize()[1] - 1);
-    if (!win.isVisible()) win.show();
-    else win.hide();
+    if (!win.isVisible()) {
+      win.show();
+    } else {
+      win.hide();
+    }
   });
 
   trayIcon.on('right-click', (bounds) => {
@@ -67,6 +87,9 @@ app.on('ready', () => {
       y: bounds.y
     });
   });
-});
 
-app.on('window-all-closed', app.quit);
+  // Declare some event to trigger them later in HTML
+  ipcMain.on('clickReload', () => win.loadURL(`file://${__dirname}/views/index.html`));
+  ipcMain.on('clickMin', () => win.hide());
+  ipcMain.on('clickClose', () => app.quit());
+});
