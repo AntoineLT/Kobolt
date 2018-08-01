c15a19bc406890d495b8c65686dcafc729f59b85
antoinelt <antoine.latorre.61@gmail.com>
2018-08-01 12:19:19 +0200

    Start of Electron app dev: - Add a new tray icon in src/img folder - Remove unused entry from locales - Change main.js to add Electron app - Rename "img" root folder to "psd"

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 'use strict';
 const electron = require('electron');
-const i18n     = require('i18n');
+const i18n = require('i18n');
 const {
   app,
   BrowserWindow,
@@ -10,12 +10,63 @@ const {
 } = electron;
 
 i18n.configure({
-    locales:['en', 'fr'],
-    defaultLocale: 'en',
-    updateFiles: false,
-    objectNotation: true,
-    directory: __dirname + '/locales'
+  locales: ['en', 'fr'],
+  defaultLocale: 'en',
+  updateFiles: false,
+  objectNotation: true,
+  directory: __dirname + '/locales'
 });
 
-i18n.setLocale('fr');
-console.log(i18n.__("tray.contextMenu.restart"));
+// How to set locale of the app context and use JSON dictionnary
+// i18n.setLocale('fr');
+// console.log(i18n.__("tray.contextMenu.restart"));
+
+app.on('ready', () => {
+  const {
+    width,
+    height
+  } = electron.screen.getPrimaryDisplay().workAreaSize;
+  const trayIcon = new Tray('src/img/tray.png');
+  const win = new BrowserWindow({
+    icon: 'src/img/tray.png',
+    width: 300,
+    height: 400,
+    resizable: false,
+    show: false,
+    alwaysOnTop: true,
+    skipTaskbar: true,
+    frame: false
+  });
+
+  let contextMenu = new Menu();
+  contextMenu.append(new MenuItem({
+    type: 'normal',
+    label: i18n.__("tray.contextMenu.restart"),
+    click: () => {
+      app.relaunch();
+      app.quit();
+    }
+  }));
+  contextMenu.append(new MenuItem({
+    type: 'normal',
+    label: i18n.__("tray.contextMenu.close"),
+    click: () => {
+      app.quit();
+    }
+  }));
+
+  trayIcon.on('click', () => {
+    win.setPosition(width - win.getSize()[0] - 1, height - win.getSize()[1] - 1);
+    if (!win.isVisible()) win.show();
+    else win.hide();
+  });
+
+  trayIcon.on('right-click', (bounds) => {
+    trayIcon.popUpContextMenu(contextMenu, {
+      x: bounds.x,
+      y: bounds.y
+    });
+  });
+});
+
+app.on('window-all-closed', app.quit);
